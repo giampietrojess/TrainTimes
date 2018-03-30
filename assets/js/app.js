6e4292bd0f72e7d50e795616ae66fe5975692a92
Jess Giampietro <JessGiampietro@Jesss-MacBook-Pro.local>
2018-03-29 23:43:09 -0400

    add initialize firebase coding

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB0VF3hGCz6rnW1ZzIBQEQr9tGV8fUAzMs",
+    authDomain: "train-times-39021.firebaseapp.com",
+    databaseURL: "https://train-times-39021.firebaseio.com",
+    projectId: "train-times-39021",
+    storageBucket: "",
+    messagingSenderId: "243609837543"
+  };
+    firebase.initializeApp(config);
